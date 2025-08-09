89042e814ae1269989ef4c7bebc6669da7f58476
HTG HUZAIFA <htghuzaifa2@gmail.com>
2025-08-09 23:20:38 +0000

    2025-08-09T23:18:31.073995532Z	Cloning repository... 2025-08-09T23:18:31

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -20,7 +20,7 @@ function SearchPageFallback() {
                 <Skeleton className="h-6 w-1/2" />
               </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
               <div className="space-y-2">
                 <Skeleton className="h-6 w-16" />
                 <Skeleton className="h-4 w-full" />
